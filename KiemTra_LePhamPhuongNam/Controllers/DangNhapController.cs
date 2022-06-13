ec56ed97b82e81afa083b650c5ffc944e9eab771
phuongnamlp <90029997+phuongnamlp@users.noreply.github.com>
2022-06-13 18:53:16 +0700

    Add files via upload

---
+++ KiemTra_LePhamPhuongNam/Controllers/DangNhapController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KiemTra_LePhamPhuongNam.Models;
+
+namespace KiemTra_LePhamPhuongNam.Controllers
+{
+    public class DangNhapController : Controller
+    {
+        // GET: DangNhap
+        Test01DataContext data = new Test01DataContext();
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DangNhap(string id, FormCollection collection, SinhVien sv)
+        {
+            if (ModelState.IsValid)
+            {
+                var f_MaSV = collection["MaSv"] ;
+                var E_data = data.SinhViens.Where(s => s.MaSV.Equals(f_MaSV)).ToList();
+                if (data != null)
+                {
+                   
+                    return RedirectToAction("ListSinhVien");
+                }
+                else
+                {
+                    ViewBag.error = "Login failed";
+                    return RedirectToAction("Login");
+                }
+            }
+            return this.DangNhap();
+        }
+    }
+}
---
+++ KiemTra_LePhamPhuongNam/Controllers/HocPhanController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KiemTra_LePhamPhuongNam.Models;
+
+
+namespace KiemTra_LePhamPhuongNam.Controllers
+{
+    public class HocPhanController : Controller
+    {
+        // GET: HocPhan
+        Test01DataContext data = new Test01DataContext();
+        public ActionResult ListHocPhan()
+        {
+            var all_hocphan = from ss in data.HocPhans select ss;
+            return View(all_hocphan);
+        }
+    }
+}
